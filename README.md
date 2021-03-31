b61876de38eabf4d58a77901110790f521a5fb43
qingbing <780042175@qq.com>
2021-03-31 17:05:13 +0800

    数据验证片段

--- README.md
+++ README.md
@@ -32,6 +32,7 @@ yii 公用的一些基础类库
 ### 片段
 1. [响应处理片段 : Response](doc/traits/TResponse.md)
 1. [制作保存失败抛出异常片段 : TSave](doc/traits/TSave.md)
+1. [数据验证片段 : TValidator](doc/traits/TValidator.md)
 
 
 # ====== 组件编号 102 ======
---
+++ src/traits/TValidator.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @link        http://www.phpcorner.net
+ * @author      qingbing<780042175@qq.com>
+ * @copyright   Chengdu Qb Technology Co., Ltd.
+ */
+
+namespace YiiHelper\traits;
+
+
+use YiiHelper\helpers\DynamicModel;
+use Zf\Helper\Exceptions\ParameterException;
+
+/**
+ * 数据验证片段
+ *
+ * Trait TValidator
+ * @package YiiHelper\traits
+ */
+trait TValidator
+{
+    /**
+     * 将参数放入验证规则进行规则验证
+     *
+     * @param array $rules
+     * @return array
+     * @throws ParameterException
+     * @throws \yii\base\InvalidConfigException
+     */
+    protected function validateParams($rules = [], $labels = [])
+    {
+        // 数据获取
+        $request = \Yii::$app->getRequest();
+        $data    = array_merge($request->getQueryParams(), $request->getBodyParams());
+        if (empty($rules)) {
+            return $data;
+        }
+        // 验证并返回数据
+        if ($this->validate($data, $rules, $labels)) {
+            return $data;
+        }
+    }
+
+    /**
+     * 动态验证数据
+     *
+     * @param array $data
+     * @param array $rules
+     * @param array $labels
+     * @return bool
+     * @throws ParameterException
+     * @throws \yii\base\InvalidConfigException
+     */
+    protected function validate(array $data, $rules = [], $labels = [])
+    {
+        if (empty($rules)) {
+            return true;
+        }
+        $model = DynamicModel::validateData($data, $rules, $labels);
+        if ($model->hasErrors()) {
+            // 验证失败
+            $error = $model->getErrorSummary(false);
+            throw new ParameterException(reset($error), 10000);
+        }
+        return true;
+    }
+}
