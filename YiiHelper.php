2e8fb75607190804d111ef2bf243e380670bdac0
qingbing <780042175@qq.com>
2021-03-31 15:25:40 +0800

    yii常用提示

---
+++ YiiHelper.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link        http://www.phpcorner.net
+ * @author      qingbing<780042175@qq.com>
+ * @copyright   Chengdu Qb Technology Co., Ltd.
+ */
+
+namespace yii {
+
+}
+
+namespace yii\console {
+
+}
+
+namespace yii\web {
+    /**
+     * Class Application
+     * @package yii\web
+     *
+     * @property-read $user \YiiHelper\components\User
+     * @method \YiiHelper\components\User getUser()
+     */
+    class Application
+    {
+    }
+}
